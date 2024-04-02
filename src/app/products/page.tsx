f364819391de9164d5c2384133374677f5811d59
Tomas Lugo <lugoleandro@hotmail.com>
2024-04-02 18:51:50 -0300

    2nd try suspense

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,7 @@
 import Filters from '@/components/Pages/Products/Filters'
 import ProductsShowcase from '@/components/Pages/Products/ProductsShowcase'
+import { Skeleton } from '@/components/ui/skeleton'
+import { Suspense } from 'react'
 
 const page = (): React.ReactElement => {
   return (
@@ -7,7 +9,21 @@ const page = (): React.ReactElement => {
       <aside className="hidden h-fit flex-col justify-center gap-4 bg-primary p-4 text-white md:flex">
         <Filters />
       </aside>
-      <ProductsShowcase />
+      <Suspense
+        fallback={[...Array(5)].map((_, index) => (
+          <div key={index} className="p-8">
+            <div className="relative flex aspect-square items-center justify-center pb-2">
+              <Skeleton className="h-full w-full" />
+            </div>
+            <div className="space-y-4 text-black">
+              <Skeleton className="h-4 w-48" />
+              <Skeleton className="h-4 w-12" />
+            </div>
+          </div>
+        ))}
+      >
+        <ProductsShowcase />
+      </Suspense>
     </main>
   )
 }
--- src/components/Pages/Products/ProductsShowcase.tsx
+++ src/components/Pages/Products/ProductsShowcase.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import ProductCardGrid from './ProductCardGrid'
-import { Suspense, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import ProductCardColumn from './ProductCardColumn'
 import ProductsOrder from './ProductsOrder'
 import { useAutoAnimate } from '@formkit/auto-animate/react'
@@ -14,7 +14,6 @@ const ProductsShowcase = (): React.ReactElement => {
   const [category, setCategory] = useState('')
   const [sort, setSort] = useState('desc')
   const searchParams = useSearchParams()
-  console.log(searchParams.get('category'))
   const { data: products, isLoading } = useFetch(
     'https://fakestoreapi.com/products',
   )
@@ -41,80 +40,67 @@ const ProductsShowcase = (): React.ReactElement => {
   return (
     <section className="min-h-screen w-full">
       <ProductsOrder toggleGridStyle={toggleGridStyle} sortFn={handleSort} />
-      <Suspense
-        fallback={[...Array(5)].map((_, index) => (
-          <div key={index} className="p-8">
-            <div className="relative flex aspect-square items-center justify-center pb-2">
-              <Skeleton className="h-full w-full" />
-            </div>
-            <div className="space-y-4 text-black">
-              <Skeleton className="h-4 w-48" />
-              <Skeleton className="h-4 w-12" />
-            </div>
-          </div>
-        ))}
-      >
-        <div ref={parent} className={gridStyleClass}>
-          {isLoading
-            ? [...Array(5)].map((_, index) => (
-                <div key={index} className="p-8">
-                  <div className="relative flex aspect-square items-center justify-center pb-2">
-                    <Skeleton className="h-full w-full" />
-                  </div>
-                  <div className="space-y-4 text-black">
-                    <Skeleton className="h-4 w-48" />
-                    <Skeleton className="h-4 w-12" />
-                  </div>
+
+      <div ref={parent} className={gridStyleClass}>
+        {isLoading
+          ? [...Array(5)].map((_, index) => (
+              <div key={index} className="p-8">
+                <div className="relative flex aspect-square items-center justify-center pb-2">
+                  <Skeleton className="h-full w-full" />
                 </div>
-              ))
-            : category === ''
-              ? (products as Product[])
-                  .sort((a, b) => {
-                    switch (sort) {
-                      case 'asc':
-                        return a.title < b.title ? -1 : 1
-                      case 'des':
-                        return b.title < a.title ? -1 : 1
-                      case 'minPrice':
-                        return a.price - b.price
-                      case 'maxPrice':
-                        return b.price - a.price
-                      default:
-                        return 0
-                    }
-                  })
-                  .map((prod, index) =>
-                    gridStyle ? (
-                      <ProductCardGrid key={index} product={prod} />
-                    ) : (
-                      <ProductCardColumn key={index} product={prod} />
-                    ),
-                  )
-              : (products as Product[])
-                  .filter(prod => prod.category.includes(category))
-                  .sort((a, b) => {
-                    switch (sort) {
-                      case 'asc':
-                        return a.title < b.title ? -1 : 1
-                      case 'des':
-                        return b.title < a.title ? -1 : 1
-                      case 'minPrice':
-                        return a.price - b.price
-                      case 'maxPrice':
-                        return b.price - a.price
-                      default:
-                        return 0
-                    }
-                  })
-                  .map((prod, index) =>
-                    gridStyle ? (
-                      <ProductCardGrid key={index} product={prod} />
-                    ) : (
-                      <ProductCardColumn key={index} product={prod} />
-                    ),
-                  )}
-        </div>
-      </Suspense>
+                <div className="space-y-4 text-black">
+                  <Skeleton className="h-4 w-48" />
+                  <Skeleton className="h-4 w-12" />
+                </div>
+              </div>
+            ))
+          : category === ''
+            ? (products as Product[])
+                .sort((a, b) => {
+                  switch (sort) {
+                    case 'asc':
+                      return a.title < b.title ? -1 : 1
+                    case 'des':
+                      return b.title < a.title ? -1 : 1
+                    case 'minPrice':
+                      return a.price - b.price
+                    case 'maxPrice':
+                      return b.price - a.price
+                    default:
+                      return 0
+                  }
+                })
+                .map((prod, index) =>
+                  gridStyle ? (
+                    <ProductCardGrid key={index} product={prod} />
+                  ) : (
+                    <ProductCardColumn key={index} product={prod} />
+                  ),
+                )
+            : (products as Product[])
+                .filter(prod => prod.category.includes(category))
+                .sort((a, b) => {
+                  switch (sort) {
+                    case 'asc':
+                      return a.title < b.title ? -1 : 1
+                    case 'des':
+                      return b.title < a.title ? -1 : 1
+                    case 'minPrice':
+                      return a.price - b.price
+                    case 'maxPrice':
+                      return b.price - a.price
+                    default:
+                      return 0
+                  }
+                })
+                .map((prod, index) =>
+                  gridStyle ? (
+                    <ProductCardGrid key={index} product={prod} />
+                  ) : (
+                    <ProductCardColumn key={index} product={prod} />
+                  ),
+                )}
+      </div>
     </section>
   )
 }
